795271857701b4742683eafa7888efddea0932de
subodhNagalwade <subodhnagalwade@gmail.com>
2022-09-07 11:52:31 +0530

    programme for checking wovels or consonent

---
+++ VowelOrConsonent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basiccoreproblem
+{
+    internal class VowelOrConsonent
+    {
+        public static void VowelsConsoCheck()
+        {
+            Console.WriteLine("Enter the charactor:");
+            char ch = Convert.ToChar(Console.ReadLine());
+            if (ch == 'a' || ch == 'A' || ch == 'e' || ch == 'E' || ch == 'i' || ch == 'I' || ch == 'o' || ch == 'O' || ch == 'u' || ch == 'U')
+            {
+                Console.WriteLine(ch + " is a Vowel");
+            }
+            else if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+            {
+                Console.WriteLine(ch + " is a consonant");
+            }
+            else
+            {
+                Console.WriteLine("please enter only character!");
+            }
+        }
+    }
+}
