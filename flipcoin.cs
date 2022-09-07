57e698981fd099eec79dd3c83f7b86c74322f8d0
subodhNagalwade <subodhnagalwade@gmail.com>
2022-09-07 11:43:52 +0530

    flif coin programe for find the percentage

---
+++ flipcoin.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basiccoreproblem
+{
+    internal class flipcoin
+    {
+        public static void calculatepercentages()
+        {
+            Console.WriteLine("plese enter the no of time coin flip");
+            int flipcoin=Convert.ToInt32(Console.ReadLine());
+            int headcount=0,tailcount=0;    
+            double headpercentage=0;
+            double tailpercentage=0;    
+            if (flipcoin>0)
+            {
+                for(int i=0;i<flipcoin;i++)
+                {
+                    Random random = new Random();   
+                    double number =random.NextDouble();
+                    if (number > 0.5) 
+                    {
+                        Console.WriteLine("head");
+                        headcount++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("tail");
+                        tailcount++;
+                    }
+                }
+                headpercentage = headcount * 100 / flipcoin;
+                tailpercentage = tailcount * 100 / flipcoin;
+                Console.WriteLine("head percentage"+headpercentage);
+                Console.WriteLine("tail percentage"+tailpercentage);
+            }
+            else
+            {
+                Console.WriteLine("enter positive number");
+            }
+        }
+    }
+}
