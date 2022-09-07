f594816682371dfc222955013c25387dfc1e3c22
subodhNagalwade <subodhnagalwade@gmail.com>
2022-09-07 11:50:23 +0530

    find quotient and reminder

---
+++ QuotientReminder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basiccoreproblem
+{
+    internal class QuotientReminder
+    {
+        public static void Quotandreminder()
+        {
+            Console.WriteLine("enter the dividend:");
+            double dividend = double.Parse(Console.ReadLine());
+            Console.WriteLine("enter the divisor:");
+            double divisor = double.Parse(Console.ReadLine());
+            double quotient = dividend / divisor;
+            double remainder = dividend % divisor;
+            Console.WriteLine("quotient: " + quotient);
+            Console.WriteLine("remainder: " + remainder);
+        }
+    }
+}
