b87a82f0dba7426c4057f03e9a5a8a30d142f7bb
subodhNagalwade <subodhnagalwade@gmail.com>
2022-09-07 11:44:59 +0530

    harmonic number programme added

---
+++ HarmonicNumber.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace basiccoreproblem
+{
+    internal class HarmonicNumber
+    {
+        public static void HarmonicNo()
+        {
+            Console.WriteLine("enter the nth number:");
+            int number = int.Parse(Console.ReadLine());
+            double j = 0;
+            for (double i = 1; i <= number; i++)
+            {
+                j = j + 1 / i;
+                Console.WriteLine(j);
+            }
+            Console.WriteLine("Sum of nth harmonic number" + j);
+        }
+    }
+}
