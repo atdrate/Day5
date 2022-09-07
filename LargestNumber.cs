0141c430aca0440a764bfdfff6af0199c9a58c7b
subodhNagalwade <subodhnagalwade@gmail.com>
2022-09-07 11:46:49 +0530

    find the largest number

---
+++ LargestNumber.cs
@@ -0,0 +1,41 @@
+﻿
+
+namespace basiccoreproblem
+{
+    internal class LargestNumber
+    {
+        public static void largestNo()
+        {
+            Console.WriteLine("enter the number1");
+            double number1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("enter the number2");
+            double number2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("enter the number3");
+            double number3 = double.Parse(Console.ReadLine());
+            if (number1 == number2 && number2 == number3)
+            {
+                Console.WriteLine("three numbers are equal");
+            }
+            if (number1 > number2)
+            {
+                if (number1 > number3)
+                {
+                    Console.WriteLine(number1 + " greater");
+                }
+                else
+                {
+                    Console.WriteLine(number3 + " greater");
+                }
+            }
+            else if (number2 > number3)
+            {
+                Console.WriteLine(number2 + " greater");
+            }
+            else
+            {
+                Console.WriteLine(number3 + " greater");
+            }
+
+        }
+    }
+}
