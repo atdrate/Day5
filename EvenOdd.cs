84d3ab7ead3c04a4803ccffd914d9d95b2612031
subodhNagalwade <subodhnagalwade@gmail.com>
2022-09-07 11:42:31 +0530

    to programme check fot even or odd number

---
+++ EvenOdd.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace basiccoreproblem
+{
+    internal class EvenOdd
+    {
+        public static void EvenOddCheck()
+        {
+            Console.WriteLine("enter the number");
+            int number = int.Parse(Console.ReadLine());
+            if (number % 2 == 0)
+                Console.WriteLine("even number");
+            else
+                Console.WriteLine("odd number");
+        }
+    }
+}
