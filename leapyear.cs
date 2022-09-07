5c15836bc4afbc0b35792176dbebb17a2d0e175c
subodhNagalwade <subodhnagalwade@gmail.com>
2022-09-07 11:47:38 +0530

    programme for leap year

---
+++ leapyear.cs
@@ -0,0 +1,30 @@
+﻿
+
+
+
+namespace basiccoreproblem
+{
+    internal class leapyear
+    {
+        public static void checkleapyear()
+        {
+
+            Console.WriteLine("enter the year");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if (year >= 1000 && year <= 9999)
+            {
+                if ((year % 4 == 0) && (year % 100 != 0 || (year % 400 == 0)))
+                {
+                    Console.WriteLine("leap year");
+                }
+                else
+                {
+                    Console.WriteLine("not leap year");
+                }
+            }
+            else
+            {
+                Console.WriteLine("enter valid year of four digit");
+            }
+        }   }
+}
